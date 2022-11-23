c5ab35eafb5c473f1026a781b3a3178c5d8d0623
Pankaj Dagade <106441405+PankajDagade@users.noreply.github.com>
2022-11-23 07:35:17 -0800

    Create Command.java

---
+++ Command.java
@@ -0,0 +1,15 @@
+import java.lang.*;
+
+class command
+{
+	public static void main(String Arg[])
+	{
+		System.out.println("Fist number is :"+Arg[1]);
+		System.out.println("Second number is :"+Arg[1]);
+	}
+	
+	int ans = Integer.parseInt(Arg[0])+ Integer.parseInt(Arg[1]);
+	System.out.println("Addition is :"+ans);
+		
+		
+}
