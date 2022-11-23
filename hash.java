41157a621720b22628f6c3320aff5d9418538b7a
Pankaj Dagade <106441405+PankajDagade@users.noreply.github.com>
2022-11-23 07:33:01 -0800

    Create hash.java

---
+++ hash.java
@@ -0,0 +1,16 @@
+import java.lang.*;
+class Demo
+{}
+
+class Copy
+{
+     public static void main(String arg[])throews Exception
+    {
+	     String str1 = "MArvellous";
+         String str2 = "MArvellous";
+		String str3 = "MArvellous";
+		String str4 = "MArvellous";
+	}
+	
+	
+	
